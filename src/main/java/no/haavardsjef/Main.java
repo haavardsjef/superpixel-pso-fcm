b2378c16af7df2a1342b31a4845674b1fbc058f9
Håvard Hjelmeseth <havhje@gmail.com>
2023-03-07 12:49:21 +0100

    feat: add loading of dataset

--- src/main/java/no/haavardsjef/Main.java
+++ src/main/java/no/haavardsjef/Main.java
@@ -1,7 +1,15 @@
 package no.haavardsjef;
 
+
+import no.haavardsjef.utility.DataLoader;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        DataLoader dataLoader = new DataLoader();
+        dataLoader.loadData();
     }
 }
---
+++ src/main/java/no/haavardsjef/utility/DataLoader.java
@@ -0,0 +1,35 @@
+package no.haavardsjef.utility;
+
+import us.hebi.matlab.mat.format.Mat5;
+import us.hebi.matlab.mat.format.Mat5File;
+import us.hebi.matlab.mat.types.Matrix;
+import us.hebi.matlab.mat.types.Source;
+import us.hebi.matlab.mat.types.Sources;
+
+import java.io.IOException;
+
+public class DataLoader {
+
+    public void loadData() {
+        try {
+            Source source = Sources.openFile("indian_pines_corrected.mat");
+            Mat5File file = Mat5.newReader(source).readMat();
+            Matrix matrix = file.getArray(0);
+
+            double[][][] dataCube = new double[matrix.getDimensions()[2]][matrix.getDimensions()[0]][matrix
+                    .getDimensions()[1]];
+            for (int b = 0; b < matrix.getDimensions()[2]; b++) {
+                for (int x = 0; x < matrix.getDimensions()[1]; x++) {
+                    for (int y = 0; y < matrix.getDimensions()[0]; y++) {
+                        dataCube[b][y][x] = matrix.getDouble(new int[] { y, x, b });
+                    }
+                }
+            }
+
+            System.out.println("Successfully loaded data!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
