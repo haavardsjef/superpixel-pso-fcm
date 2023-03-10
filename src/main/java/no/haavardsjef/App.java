dd9e54ee96bd082923b1eeacf47c49c4fd82d0c9
Håvard Hjelmeseth <havhje@gmail.com>
2023-03-10 14:11:04 +0100

    feat: add vizualisation functionality for PSO (2D only)

--- src/main/java/no/haavardsjef/App.java
+++ src/main/java/no/haavardsjef/App.java
@@ -10,13 +10,16 @@ public class App {
 
     public static void main(String[] args) {
         AbstractFitnessFunction fitnessFunction = new FCM(2.0f, new EuclideanDistance(), new DataLoader());
-        SwarmPopulation swarmPopulation = new SwarmPopulation(30, 2, 0, 199, fitnessFunction);
+        SwarmPopulation swarmPopulation = new SwarmPopulation(200, 2, 0, 199, fitnessFunction);
         // Start timer
         long startTime = System.nanoTime();
-        swarmPopulation.optimize(30, 0.5f, 0.5f, 0.2f);
+        swarmPopulation.optimize(20, 0.5f, 0.5f, 0.2f, true);
         // Stop timer
         long endTime = System.nanoTime();
         long duration = (endTime - startTime) / 1000000;
         System.out.println("Time: " + duration + "ms");
     }
+
+
+
 }
--- src/main/java/no/haavardsjef/pso/SwarmPopulation.java
+++ src/main/java/no/haavardsjef/pso/SwarmPopulation.java
@@ -43,9 +43,41 @@ public class SwarmPopulation {
         }
     }
 
-    public void optimize(int numIterations, float w, float c1, float c2) {
+    private void plot(int iteration){
+        List<Double> x = new ArrayList<>();
+        List<Double> y = new ArrayList<>();
+
+        for (Particle particle : this.particles) {
+            x.add((double) particle.getPosition()[0]);
+            y.add((double) particle.getPosition()[1]);
+        }
+
+        // Plot swarm using matplotlib4j
+        Plot plt = Plot.create();
+        plt.plot().add(x, y, "o");
+        // Make both x and y go from 0 to 10
+        plt.xlim(0, 200);
+        plt.ylim(0, 200);
+        plt.title("Iteration " + iteration);
+//            plt.show();
+        plt.savefig("C:/Users/Haavard/github/superpixel-pso-fcm/viz/" + iteration + ".png");
+        try {
+            plt.executeSilently();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (PythonExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Saved plot " + iteration);
+
+    }
+
+    public void optimize(int numIterations, float w, float c1, float c2, boolean plot) {
         for (int i = 0; i < numIterations; i++) {
             System.out.println("Iteration: " + i);
+            if(plot && this.numDimensions == 2){
+                plot(i);
+            }
             for (Particle particle : particles) {
                 particle.updateVelocity(globalBestPosition, w, c1, c2);
                 particle.updatePosition();
@@ -59,8 +91,10 @@ public class SwarmPopulation {
             System.out.println("Global best fitness after iteration " + i + ": " + globalBestFitness);
             System.out.println("Global best position after iteration " + i + ": " + Arrays.toString(globalBestPosition));
         }
+        if(plot && this.numDimensions == 2){
+            plot(numIterations);
+        }
         System.out.println("Global best fitness: " + globalBestFitness);
         System.out.println("Global best position: " + Arrays.toString(globalBestPosition));
     }
-
 }
