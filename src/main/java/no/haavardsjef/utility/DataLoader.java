809ed6325714dc0a973d06cd6773224a7bbfea2b
Håvard Hjelmeseth <havhje@gmail.com>
2023-03-14 09:12:30 +0100

    feat: add functionality to load ground truths

--- src/main/java/no/haavardsjef/utility/DataLoader.java
+++ src/main/java/no/haavardsjef/utility/DataLoader.java
@@ -7,45 +7,79 @@ import us.hebi.matlab.mat.types.Source;
 import us.hebi.matlab.mat.types.Sources;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class DataLoader implements IDataLoader {
 
-    private double[][][] data;
-    private double[][] dataFlatted; // Flattened pixel data
+	private double[][][] data;
+	private double[][] dataFlatted; // Flattened pixel data
 
-    public void loadData() {
-        String path = "data/indian_pines_corrected.mat";
-        try {
-            Source source = Sources.openFile(path);
-            Mat5File file = Mat5.newReader(source).readMat();
-            Matrix matrix = file.getArray(0);
+	private int[] groundTruthFlattened;
 
-            double[][][] dataCube = new double[matrix.getDimensions()[2]][matrix.getDimensions()[0]][matrix
-                    .getDimensions()[1]];
-            this.dataFlatted = new double[matrix.getDimensions()[2]][matrix.getDimensions()[0] * matrix.getDimensions()[1]];
+	public void loadData() {
+		String path = "data/indian_pines_corrected.mat";
+		try {
+			Source source = Sources.openFile(path);
+			Mat5File file = Mat5.newReader(source).readMat();
+			Matrix matrix = file.getArray(0);
 
-            for (int b = 0; b < matrix.getDimensions()[2]; b++) {
-                for (int x = 0; x < matrix.getDimensions()[1]; x++) {
-                    for (int y = 0; y < matrix.getDimensions()[0]; y++) {
-                        dataCube[b][y][x] = matrix.getDouble(new int[] { y, x, b });
-                        dataFlatted[b][y * matrix.getDimensions()[1] + x] = matrix.getDouble(new int[] { y, x, b });
-                    }
-                }
-            }
+			double[][][] dataCube = new double[matrix.getDimensions()[2]][matrix.getDimensions()[0]][matrix
+					.getDimensions()[1]];
+			this.dataFlatted = new double[matrix.getDimensions()[2]][matrix.getDimensions()[0] * matrix.getDimensions()[1]];
 
-            this.data = dataCube;
-            System.out.println("Successfully loaded data from path: " + path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+			for (int b = 0; b < matrix.getDimensions()[2]; b++) {
+				for (int x = 0; x < matrix.getDimensions()[1]; x++) {
+					for (int y = 0; y < matrix.getDimensions()[0]; y++) {
+						dataCube[b][y][x] = matrix.getDouble(new int[]{y, x, b});
+						dataFlatted[b][y * matrix.getDimensions()[1] + x] = matrix.getDouble(new int[]{y, x, b});
+					}
+				}
+			}
 
-    }
+			this.data = dataCube;
+			System.out.println("Successfully loaded data from path: " + path);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
-    public double[] getDataPoint(int index) {
-        return dataFlatted[index];
-    }
+	}
 
-    public int getNumberOfDataPoints() {
-        return dataFlatted.length;
-    }
+
+	public double[] getDataPoint(int index) {
+		return dataFlatted[index];
+	}
+
+	public int getNumberOfDataPoints() {
+		return dataFlatted.length;
+	}
+
+	// Load ground truth
+	public void loadGroundTruth() {
+		String path = "data/indian_pines_gt.mat";
+		try (Source source = Sources.openFile(path)) {
+			Mat5File file = Mat5.newReader(source).readMat();
+
+			Matrix matrix = file.getArray(0);
+			Set<Integer> classes = new HashSet<>();
+			this.groundTruthFlattened = new int[matrix.getDimensions()[0] * matrix.getDimensions()[1]];
+			for (int i = 0; i < matrix.getDimensions()[0]; i++) {
+				for (int j = 0; j < matrix.getDimensions()[1]; j++) {
+					int classLabel = (int) matrix.getDouble(i, j);
+					this.groundTruthFlattened[i * matrix.getDimensions()[1] + j] = classLabel;
+					classes.add(classLabel);
+				}
+			}
+			System.out.println("Successfully loaded ground truth from path: " + path);
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public int[] getGroundTruthFlattened() {
+		return groundTruthFlattened;
+	}
+
 }
--- src/test/java/DataLoaderTest.java
+++ src/test/java/DataLoaderTest.java
@@ -6,13 +6,28 @@ import static org.junit.Assert.assertEquals;
 
 public class DataLoaderTest {
 
-    @Test
-    public void testLoadIndianPines() {
-        DataLoader dataLoader = new DataLoader();
-        dataLoader.loadData();
-        assertEquals(200, dataLoader.getNumberOfDataPoints()); // Check number of bands is correct
-        assertEquals(145*145, dataLoader.getDataPoint(0).length); // Check number of pixels is correct
-        assertEquals(3172, dataLoader.getDataPoint(0)[0], 0.0001); // Check first pixel value is correct
-        assertEquals(2580, dataLoader.getDataPoint(0)[1], 0.0001); // Check second pixel value is correct
-    }
+	@Test
+	public void testLoadIndianPines() {
+		DataLoader dataLoader = new DataLoader();
+		dataLoader.loadData();
+		assertEquals(200, dataLoader.getNumberOfDataPoints()); // Check number of bands is correct
+		assertEquals(145 * 145, dataLoader.getDataPoint(0).length); // Check number of pixels is correct
+		assertEquals(3172, dataLoader.getDataPoint(0)[0], 0.0001); // Check first pixel value is correct
+		assertEquals(2580, dataLoader.getDataPoint(0)[1], 0.0001); // Check second pixel value is correct
+	}
+
+	@Test
+	public void testLoadIndianPinesGroundTruth() {
+		DataLoader dataLoader = new DataLoader();
+		dataLoader.loadGroundTruth();
+		int[] groundTruth = dataLoader.getGroundTruthFlattened();
+
+		assertEquals(145 * 145, groundTruth.length); // Check number of pixels is correct
+		assertEquals(3, groundTruth[0]); // Check first pixel value is correct
+		assertEquals(3, groundTruth[1]); // Check second pixel value is correct
+		assertEquals(0, groundTruth[20]); // Check 21st pixel value is correct
+		assertEquals(3, groundTruth[145]); // Check first pixel on 2nd row is correct
+
+	}
+
 }
