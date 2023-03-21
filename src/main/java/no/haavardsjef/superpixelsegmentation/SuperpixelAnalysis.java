94f1ff9d162970724ad9f11039c851a3881fa148
Håvard Hjelmeseth <havhje@gmail.com>
2023-03-21 14:14:07 +0100

    feat: add methods to calculate mean superpixel values per spectral bands

--- src/main/java/no/haavardsjef/superpixelsegmentation/SuperpixelAnalysis.java
+++ src/main/java/no/haavardsjef/superpixelsegmentation/SuperpixelAnalysis.java
@@ -36,3 +36,38 @@ public class SuperpixelAnalysis {
 	public int getNumberOfSuperpixels() {
 		return numberOfSuperpixels;
 	}
+
+	/**
+	 * Calculate the mean value in a given superpixel for the given band data.
+	 *
+	 * @param superpixelIndex - The index of the superpixel we wish to calculate the mean of.
+	 * @param bandData        - The flattened pixel values from a single band.
+	 * @return - The mean value for the given superpixel and band.
+	 */
+	public double meanValueInSuperpixel(int superpixelIndex, double[] bandData) {
+		if (superpixelMap.length != bandData.length) {
+			throw new IllegalArgumentException("The superpixel map and the band data must have the same length.");
+		}
+		// Sum all pixel values for the specific band if in correct superpixel
+		int[] pixelIndexes = IntStream.range(0, bandData.length).filter(i -> superpixelMap[i] == superpixelIndex).toArray();
+		double totalValue = Arrays.stream(pixelIndexes).mapToObj(i -> bandData[i]).reduce(0.0, (a, b) -> a + b).doubleValue();
+
+		return totalValue / pixelIndexes.length;
+	}
+
+	/**
+	 * Calculates the mean value in all superpixels from the given band data.
+	 *
+	 * @param bandData - The flattened pixel values from a single band.
+	 * @return - An array containing the mean value for each superpixel across that band.
+	 */
+	public double[] meanValueInAllSuperpixels(double[] bandData) {
+		if (superpixelMap.length != bandData.length) {
+			throw new IllegalArgumentException("The superpixel map and the band data must have the same length.");
+		}
+		double[] meanValues = new double[numberOfSuperpixels];
+		for (int i = 0; i < numberOfSuperpixels; i++) {
+			meanValues[i] = meanValueInSuperpixel(i, bandData);
+		}
+		return meanValues;
+	}
