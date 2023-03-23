e740b744cee163e5c7cbd85a1c7ce6b82def35c2
Håvard Hjelmeseth <havhje@gmail.com>
2023-03-23 16:21:30 +0100

    feat: update FCM to use distance metric from Dataset

--- src/main/java/no/haavardsjef/fcm/FuzzyCMeans.java
+++ src/main/java/no/haavardsjef/fcm/FuzzyCMeans.java
@@ -6,7 +6,6 @@ import no.haavardsjef.DatasetName;
 import no.haavardsjef.objectivefunctions.IObjectiveFunction;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.indexing.NDArrayIndex;
 import org.nd4j.linalg.ops.transforms.Transforms;
 
 import java.io.IOException;
@@ -14,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 import java.util.stream.IntStream;
 
 @Log4j2
@@ -31,20 +31,56 @@ public class FuzzyCMeans implements IObjectiveFunction {
 	}
 
 
-	public INDArray calculateMembershipMatrix(INDArray candidateCentroids) {
+//	public INDArray calculateMembershipMatrix(INDArray candidateCentroids) {
+//		int numDataPoints = (int) data.size(0);
+//		int numClusters = (int) candidateCentroids.size(0);
+//		INDArray candidateMembershipMatrix = Nd4j.zeros(numDataPoints, numClusters);
+//		double epsilon = 1e-9;
+//
+//		IntStream.range(0, numDataPoints).parallel().forEach(i -> {
+//			INDArray dataPoint = data.get(NDArrayIndex.point(i), NDArrayIndex.all(), NDArrayIndex.all());
+//
+//			// Create a separate distances INDArray for each thread
+//			INDArray distances = Nd4j.create(numClusters);
+//
+//			for (int j = 0; j < numClusters; j++) {
+//				distances.putScalar(j, dataPoint.distance2(candidateCentroids.get(NDArrayIndex.point(j), NDArrayIndex.all(), NDArrayIndex.all())));
+//			}
+//			INDArray membershipDenominator = Transforms.pow(distances.reshape(1, -1).div(distances.add(epsilon)), 2.0 / (fuzziness - 1)).sum(1);
+//			candidateMembershipMatrix.putRow(i, membershipDenominator.rdiv(1.0));
+//		});
+//
+//		return candidateMembershipMatrix;
+//	}
+//
+//
+//	public double objectiveFunction(INDArray candidateCentroids) {
+//		INDArray candidateMembershipMatrix = calculateMembershipMatrix(candidateCentroids);
+//
+//		int numDataPoints = (int) data.size(0);
+//		int numClusters = (int) candidateCentroids.size(0);
+//
+//		return IntStream.range(0, numDataPoints).parallel().mapToDouble(i -> {
+//			double sum = 0.0;
+//			for (int j = 0; j < numClusters; j++) {
+//				double membership = Math.pow(candidateMembershipMatrix.getDouble(i, j), fuzziness);
+//				double distance = data.get(NDArrayIndex.point(i), NDArrayIndex.all(), NDArrayIndex.all()).distance2(candidateCentroids.get(NDArrayIndex.point(j), NDArrayIndex.all(), NDArrayIndex.all()));
+//				sum += membership * distance;
+//			}
+//			return sum;
+//		}).sum();
+//	}
+
+	public INDArray calculateMembershipMatrix(List<Integer> candidateCentroids) {
 		int numDataPoints = (int) data.size(0);
-		int numClusters = (int) candidateCentroids.size(0);
+		int numClusters = candidateCentroids.size();
 		INDArray candidateMembershipMatrix = Nd4j.zeros(numDataPoints, numClusters);
 		double epsilon = 1e-9;
 
 		IntStream.range(0, numDataPoints).parallel().forEach(i -> {
-			INDArray dataPoint = data.get(NDArrayIndex.point(i), NDArrayIndex.all(), NDArrayIndex.all());
-
-			// Create a separate distances INDArray for each thread
 			INDArray distances = Nd4j.create(numClusters);
-
 			for (int j = 0; j < numClusters; j++) {
-				distances.putScalar(j, dataPoint.distance2(candidateCentroids.get(NDArrayIndex.point(j), NDArrayIndex.all(), NDArrayIndex.all())));
+				distances.putScalar(j, dataset.euclideanDistance(i, candidateCentroids.get(j)));
 			}
 			INDArray membershipDenominator = Transforms.pow(distances.reshape(1, -1).div(distances.add(epsilon)), 2.0 / (fuzziness - 1)).sum(1);
 			candidateMembershipMatrix.putRow(i, membershipDenominator.rdiv(1.0));
@@ -53,22 +89,28 @@ public class FuzzyCMeans implements IObjectiveFunction {
 		return candidateMembershipMatrix;
 	}
 
-
-	public double objectiveFunction(INDArray candidateCentroids) {
+	public double objectiveFunction(List<Integer> candidateCentroids) {
 		INDArray candidateMembershipMatrix = calculateMembershipMatrix(candidateCentroids);
 
 		int numDataPoints = (int) data.size(0);
-		int numClusters = (int) candidateCentroids.size(0);
+		int numClusters = candidateCentroids.size();
 
-		return IntStream.range(0, numDataPoints).parallel().mapToDouble(i -> {
-			double sum = 0.0;
-			for (int j = 0; j < numClusters; j++) {
+		AtomicReferenceArray<Double> atomicArray = new AtomicReferenceArray<>(numDataPoints * numClusters);
+		IntStream.range(0, numDataPoints).parallel().forEach(i -> {
+			IntStream.range(0, numClusters).parallel().forEach(j -> {
 				double membership = Math.pow(candidateMembershipMatrix.getDouble(i, j), fuzziness);
-				double distance = data.get(NDArrayIndex.point(i), NDArrayIndex.all(), NDArrayIndex.all()).distance2(candidateCentroids.get(NDArrayIndex.point(j), NDArrayIndex.all(), NDArrayIndex.all()));
-				sum += membership * distance;
-			}
-			return sum;
-		}).sum();
+				double distance = dataset.euclideanDistance(i, candidateCentroids.get(j));
+				double product = membership * distance;
+				atomicArray.set(i * numClusters + j, product);
+			});
+		});
+
+		double sum = 0.0;
+		for (int i = 0; i < atomicArray.length(); i++) {
+			sum += atomicArray.get(i);
+		}
+
+		return sum;
 	}
 
 
@@ -98,8 +140,7 @@ public class FuzzyCMeans implements IObjectiveFunction {
 			log.info("Found solution in cache. Returning cached fitness value.");
 			result = this.fitnessCache.get(candidateSolution.toString());
 		} else {
-			INDArray candidateCentroids = this.dataset.getBands(candidateSolution);
-			result = (float) this.objectiveFunction(candidateCentroids);
+			result = (float) this.objectiveFunction(candidateSolution);
 
 			// Add to cache
 			this.fitnessCache.put(candidateSolution.toString(), result);
