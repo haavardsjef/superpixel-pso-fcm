ec1761076b679e96a004ab8960090c7ac18c6c31
Håvard Hjelmeseth <havhje@gmail.com>
2023-03-30 12:41:27 +0200

    chore: make Sample record serializable

--- src/main/java/no/haavardsjef/classification/Sample.java
+++ src/main/java/no/haavardsjef/classification/Sample.java
@@ -1,6 +1,8 @@
 package no.haavardsjef.classification;
 
-public record Sample (int pixelIndex, int label, double[] features) {
+import java.io.Serializable;
+
+public record Sample(int pixelIndex, int label, double[] features) implements Serializable {
 
 }
 
