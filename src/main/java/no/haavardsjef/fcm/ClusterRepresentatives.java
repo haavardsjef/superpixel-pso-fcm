f15fd629b04e41a64fc4ab25b542b6a5d87438b8
Håvard Hjelmeseth <havhje@gmail.com>
2023-04-01 14:24:00 +0200

    feat: add hard clustering to ClusterRepresentatives class

--- src/main/java/no/haavardsjef/fcm/ClusterRepresentatives.java
+++ src/main/java/no/haavardsjef/fcm/ClusterRepresentatives.java
@@ -1,8 +1,11 @@
 package no.haavardsjef.fcm;
 
 import no.haavardsjef.dataset.Dataset;
+import org.nd4j.linalg.api.ndarray.INDArray;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Class providing methods to select cluster representatives given a set of cluster centers
@@ -10,9 +13,35 @@ import java.util.List;
 public class ClusterRepresentatives {
 
 	private final Dataset dataset;
+	private final List<List<Integer>> clusters;
 
 	public ClusterRepresentatives(Dataset dataset) {
 		this.dataset = dataset;
+		clusters = new ArrayList<>();
+	}
+
+	public void hardClusterBands(List<Integer> clusterCentroids) {
+		int numClusters = clusterCentroids.size();
+
+		for (int i = 0; i < numClusters; i++) {
+			clusters.add(new ArrayList<>());
+		}
+
+		// For each band, find the closest cluster centroid and add it to the cluster
+		for (int i = 0; i < dataset.getNumBands(); i++) {
+			int closestCentroid = 0;
+			double closestDistance = Double.MAX_VALUE;
+
+			for (int j = 0; j < numClusters; j++) {
+				double distance = dataset.euclideanDistance(i, clusterCentroids.get(j));
+				if (distance < closestDistance) {
+					closestCentroid = j;
+					closestDistance = distance;
+				}
+			}
+
+			clusters.get(closestCentroid).add(i);
+		}
 	}
 
 
